0f7e0e1ec59343010f186c9324b081adc31a3ddf
Deepshikha Tiwari <shikhablb1@gmail.com>
2021-04-13 17:28:20 +0530

    Create model bookmark

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,2 +1,16 @@
 from django.db import models
+from django.urls import reverse
+from taggit.managers import TaggableManager
 
+
+class bookmark(models.Model):
+    url = models.URLField(max_length=200)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200, help_text='Add description of book')
+    tag = TaggableManager()
+
+    def get_absolute_url(self):
+        return reverse('bookmark-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return self.name
