1a7c15f2a7fc224327486792167da0a37d2fb963
Deepshikha Tiwari <shikhablb1@gmail.com>
2021-04-13 17:29:13 +0530

    Create detail and list view using generic

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+from .models import bookmark
 
 
+def index(request):
+    return render(request, 'index.html')
+
+
+class BookmarkDetailView(generic.DetailView):
+    model = bookmark
+    paginate_by = 5
+
+
+class BookmarkListView(generic.ListView):
+    model = bookmark
+    paginate_by = 5
+
+
+class CreateBookmark(generic.CreateView):
+    model = bookmark
+    fields = {'url', 'name', 'description', 'tag'}
+
+
+class UpdateBookmark(generic.UpdateView):
+    model = bookmark
+    fields = '__all__'
+    context_object_name = 'bookmarks'
+
+
+class DeleteBookmark(generic.DeleteView):
+    model = bookmark
+    context_object_name = 'bookmarks'
+    success_url = reverse_lazy('bookmark')
