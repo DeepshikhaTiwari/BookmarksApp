181aa2aed45f5dd4745025fd3e322d50d03ee2c7
Deepshikha Tiwari <shikhablb1@gmail.com>
2021-04-23 10:54:32 +0530

    Make the url field unique

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -4,7 +4,7 @@ from taggit.managers import TaggableManager
 
 
 class bookmark(models.Model):
-    url = models.URLField(max_length=200)
+    url = models.URLField(max_length=200, unique=True)
     name = models.CharField(max_length=200)
     description = models.TextField(max_length=1000, help_text='Add description of bookmark')
     tag = TaggableManager()
